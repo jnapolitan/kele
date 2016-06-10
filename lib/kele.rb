820f96cf81d7c40156d6985f8794fd472b769a2e
Julian Napolitan <jnapolitan@twitter.com>
2016-06-09 19:44:16 -0700

    return user data as ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
 
   def initialize(email, password)
     @api_url = 'https://www.bloc.io/api/v1/'
@@ -16,5 +18,10 @@ class Kele
     response = HTTParty.post("#{@api_url}/sessions", @options)
     @auth_token = response["auth_token"]
     puts 'ERROR: INVALID CREDENTIALS' if @auth_token == nil
+  end
+
+  def get_me
+    response = self.class.get("#{@api_url}/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 end
